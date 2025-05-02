67db3bdca96e66acd47940f38a4e319251968ebc
Dhruv Jain <dhruv.jain@tothenew.com>
2025-05-02 14:26:33 +0530

    Initial commit from Create Next App

---
+++ app/api/projects/route.js
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+import { Client } from 'pg';
+export async function  GET(){
+    const client = new Client({
+        connectionString: process.env.DATABASE_URL,
+    });
+    try{
+        await client.connect();
+        const result= await client.query('SELECT * from projects');
+        return NextResponse.json(result.rows);
+    } catch(err){
+        return NextResponse.json({ status: 'error', error: err.message });
+    }
+}
